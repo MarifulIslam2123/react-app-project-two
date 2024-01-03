a03ae8cae40053f5e9c280f70c37aa5723483deb
MarifulIslam2123 <remudhaka@gmail.com>
2024-01-04 01:04:00 +0600

    Card add

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,16 @@ function App() {
           <li className="step step-neutral">end</li>
         </ul>
       </div>
+
+
+
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+      </div>
     </>
   )
 }
